051df1cc1d019506de21eea20fc9ad093c402474
unknown <gonzalezleonel@gmail.com>
2024-11-26 08:41:23 -0600

    Crear un metodo para jalar el html

--- routes/login_routes.py
+++ routes/login_routes.py
@@ -5,13 +5,17 @@ from utils.hash_util import check_password
 
 usuario_bp = Blueprint("login", __name__)
 
-@usuario_bp.route("/", methods=['GET', 'POST'])
+@usuario_bp.route("/", methods=["GET","POST"])
 def login():
-    data = request.get_json()
+    if not request.is_json:  # Verifica si el contenido es JSON
+        return jsonify({"msg": "El contenido debe ser JSON"}), 415
+
+    data = request.get_json()  # Obtén el JSON del cuerpo de la solicitud
     usuario = Usuario.obtener_usuario_por_email(data["email"])
     if not usuario or not check_password(data["password"], usuario["password"]):
         return jsonify({"msg": "Credenciales inválidas"}), 401
 
+    # Crear el token con los datos del usuario
     token = create_access_token(identity={
         "id": str(usuario["_id"]),
         "rol": usuario["rol"],
@@ -19,3 +23,4 @@ def login():
         "departamento_id": usuario.get("departamento_id")
     })
     return jsonify({"token": token})
+
