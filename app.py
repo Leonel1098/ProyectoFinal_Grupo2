728174fbf8d27dd44eca8bf632de009337e6b889
unknown <gonzalezleonel@gmail.com>
2024-11-28 09:37:38 -0600

    Modificacion de Puerto

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask_socketio import SocketIO
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask_jwt_extended import JWTManager
-from config import Config, mongo,jwt,socket_io
+from config import Config, mongo, jwt, socket_io
 from flask_cors import CORS
-
+import os
 
 # Inicializar aplicación
 app = Flask(__name__)
@@ -23,8 +23,7 @@ from routes.empresa_routes import empresa_bp
 from routes.departamento_routes import departamento_bp
 from routes.tareas_routes import tarea_bp
 
-
-app.register_blueprint(usuario_bp,url_prefix="/usuarios")
+app.register_blueprint(usuario_bp, url_prefix="/usuarios")
 app.register_blueprint(empresa_bp, url_prefix="/empresas")
 app.register_blueprint(departamento_bp, url_prefix="/departamento")
 app.register_blueprint(tarea_bp, url_prefix="/tareas")
@@ -36,4 +35,8 @@ def handle_nuevo_usuario(data):
     socket_io.emit('nuevo_usuario', data)
 
 if __name__ == "__main__":
-    socket_io.run(app, debug=True)
+    # Obtener el puerto desde la variable de entorno de Render
+    port = int(os.environ.get('PORT', 5000))  # Puerto por defecto 5000 si no se encuentra el 'PORT'
+    
+    # Ejecutar la aplicación Flask con SocketIO
+    socket_io.run(app, host='0.0.0.0', port=port, debug=True)
