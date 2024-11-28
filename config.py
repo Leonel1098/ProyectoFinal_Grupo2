e9c25e9d846f38c4aaa9617f2638e07d7d74c08e
unknown <gonzalezleonel@gmail.com>
2024-11-28 11:51:29 -0600

    Funcion en despliegue

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super_secret_key")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://gonzalezleonel1098:Leonel10@cluster0.uapuf.mongodb.net/?tls=true")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://gonzalezleonel1098:Leonel10@cluster0.uapuf.mongodb.net/Gestion_Tareas?tls=true")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt_secret_key")
 
 
