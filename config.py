5bf30a60544cccaa218c35432367be8b6361b679
unknown <gonzalezleonel@gmail.com>
2024-11-28 10:32:55 -0600

    Modificacion ruta base de datos

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super_secret_key")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://gonzalezleonel1098:Leonel10@cluster0.uapuf.mongodb.net/Gestion_Tareas")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://gonzalezleonel1098:Leonel10@cluster0.uapuf.mongodb.net/?tls=true")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt_secret_key")
 
 
